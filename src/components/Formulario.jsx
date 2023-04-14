7c53e47a8e5305988bc22dc3984cabc0a7384f13
EliasGonzalezCarbajal <eliasgcarbajal@gmail.com>
2023-04-14 14:38:34 -0600

    actualizacion de  mensaje de error en campos de formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,6 +2,25 @@ import { useState, useEffect } from "react";
 
 const Formulario = () => {
   const [nombre, setNombre] = useState('');
+  const [propietario, setPropietario] = useState('');
+  const [email, setEmail] = useState('');
+  const [fecha, setFecha] = useState('');
+  const [sintomas, setSintomas] = useState('');
+
+  const [error, setError] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //Validacion del formulario
+    if ([ nombre, propietario, email, fecha, sintomas ].includes('') ) { 
+      console.log('Llenar todos los campos por favor')
+
+      setError(true)
+      return;
+    } 
+    setError(false)
+  }
 
 
   return (
@@ -11,10 +30,19 @@ const Formulario = () => {
           Añade Pacientes y {''}
           <span className="text-indigo-600 font-bold">Administralos</span>
         </p>
-        <form className="bg-white shadow-md rounded-lg py-10 px-5">
+
+        <form 
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded-lg py-10 px-5">
+        
+        { error && 
+        <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md" >
+          <p>Todos los campos son obligatorios</p>
+          </div>
+          }
           <div className="mb-5">
             <label htmlFor="mascota" className="block text-gray-700 font-bold uppercase">
-              Nombre Mascota
+              Nombre Mascota 
               </label>
             <input 
             id="mascota"
@@ -22,7 +50,7 @@ const Formulario = () => {
             placeholder="Nombre de la Mascota"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
             value={nombre}
-            onChange={(e) => console.log('Escribiendo...')}
+            onChange={ (e) => setNombre(e.target.value) }
             />
           </div>
           <div className="mb-5">
@@ -34,6 +62,8 @@ const Formulario = () => {
             type="text"
             placeholder="Nombre del Propietario"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+            value={propietario}
+            onChange={ (e) => setPropietario(e.target.value) }
             />
           </div>
           <div className="mb-5">
@@ -45,6 +75,8 @@ const Formulario = () => {
             type="email"
             placeholder="Email Contacto Propietario"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+            value={email}
+            onChange={ (e) => setEmail(e.target.value) }
             />
           </div>
           <div className="mb-5">
@@ -55,20 +87,23 @@ const Formulario = () => {
             id="alta"
             type="date"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+            value={fecha}
+            onChange={ (e) => setFecha(e.target.value) }
             />
           </div>
           <div className="mb-5">
             <label htmlFor="propietario" className="block text-gray-700 font-bold uppercase">
-              Nombre Propietario
+              Sintomas
               </label>
             <textarea 
             id="sintomas"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
             placeholder="Describe los sintomas"
-            
-   
+            value={sintomas}
+            onChange={ (e) => setSintomas(e.target.value) }
             />
           </div>
+
           <input 
           type="submit"
           className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-300 cursor-pointer transition-opacity " 
